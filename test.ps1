91d9e48c06d0a4019ec49df66e1e6a23f194995a
jackyjngwn <jackyjngwn@yahoo.com>
2016-11-02 14:52:44 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,3 @@
 param([string]$key)
 net use P: \\azurehpcstorage.file.core.windows.net\hpcfilestorage  /u:azurehpcstorage $key
-cp P:\w_mpi-rt_p_5.1.3.180.exe C:\Users\lzhuo
+cp P:\w_mpi-rt_p_5.1.3.180.exe C:\Users\Public
